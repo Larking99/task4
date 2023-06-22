5195facc26f83dea04db87774c46372343a0c278
Larking99 <engineerlark@gmail.com>
2023-06-22 15:23:50 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,111 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="container py-5 my-5">
+    <h4 v-if="completedTasks.length > 0" class="list-group list-group-numbered">
+      Completed Tasks
+    </h4>
+    <ol v-if="completedTasks.length > 0" class="list-group list-group-numbered">
+      <li
+        v-for="(task, i) in completedTasks"
+        :key="task.name + '-' + i"
+        class="list-group-item d-flex justify-content-between align-items-start"
+        :class="{ 'list-group-item-success': task.completed }"
+      >
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">
+            {{ task.completed ? "Completed" : "Not Completed" }}
+          </div>
+          {{ task.name }}
+        </div>
+        <input type="checkbox" v-model="task.completed" />
+      </li>
+    </ol>
+
+    <template v-if="pendingTasks.length > 0">
+      <h4 class="mt-5">Pending Tasks</h4>
+      <ol class="list-group list-group-numbered">
+        <li
+          v-for="(task, i) in pendingTasks"
+          :key="task.name + '-' + i"
+          class="list-group-item d-flex justify-content-between align-items-start list-group-item-warning"
+        >
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">
+              {{ task.completed ? "Completed" : "Not Completed" }}
+            </div>
+            {{ task.name }}
+          </div>
+          <input type="checkbox" v-model="task.completed" />
+        </li>
+      </ol>
+    </template>
+
+    <input
+      type="text"
+      v-model="form.name"
+      class="form-control mt-5"
+      @keydown.enter.stop="addTodoToTasks"
+    />
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        completed: false,
+      },
+      tasks: [
+        {
+          name: "Go shopping",
+          completed: true,
+        },
+        {
+          name: "Do programming",
+          completed: false,
+        },
+        {
+          name: "Take the baby to school",
+          completed: false,
+        },
+      ],
+    };
+  },
+  created() {
+    if (localStorage.getItem("tasks")) {
+      this.tasks = JSON.parse(window.atob(localStorage.getItem("tasks")));
+    }
+  },
+  watch: {
+    tasks: {
+      handler(newValue, oldValue) {
+        localStorage.setItem("tasks", window.btoa(JSON.stringify(this.tasks)));
+      },
+      deep: true,
+    },
+  },
+  computed: {
+    completedTasks() {
+      return this.tasks.filter((task) => task.completed === true);
+    },
+    pendingTasks() {
+      return this.tasks.filter((task) => task.completed === false);
+    },
+  },
+  methods: {
+    addTodoToTasks() {
+      if (this.form.name) {
+        this.tasks.push(JSON.parse(JSON.stringify(this.form)));
+        this.form.name = "";
+      } else {
+        alert("Please provide a task");
+      }
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+/* Your styles here */
 </style>
